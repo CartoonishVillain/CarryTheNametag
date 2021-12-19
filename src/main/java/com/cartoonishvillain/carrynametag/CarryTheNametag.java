634aeee9e37e9f22f75409ef707dc3f003e19640
Cartoonish Villain <jedijoe93@gmail.com>
2021-12-19 12:38:20 -0500

    1.18

--- src/main/java/com/cartoonishvillain/carrynametag/CarryTheNametag.java
+++ src/main/java/com/cartoonishvillain/carrynametag/CarryTheNametag.java
@@ -1,23 +1,12 @@
 package com.cartoonishvillain.carrynametag;
 
 import net.minecraft.network.chat.TextComponent;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.living.BabyEntitySpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("carrynametag")
